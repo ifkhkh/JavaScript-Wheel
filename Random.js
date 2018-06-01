055b88126e1a476938b7a16c42b793321ec76d66
陈 <574682566@qq.com>
2018-06-01 23:33:09 +0800

    Update Random.js

--- Random.js
+++ Random.js
@@ -1,3 +1,4 @@
+// 取 lowerValue - upperValue 间的随机值。
 var randomNum = (lowerValue, upperValue) => {
     var choices = upperValue - lowerValue + 1
     var result = Math.floor(Math.random() * choices + lowerValue)
