9ca1f2ae920d8f70aad6873731d19588efaf5ab5
陈 <574682566@qq.com>
2019-03-06 11:26:02 +0800

    create class JSType
    compare with result of isTypeOf func

--- typeOf.js
+++ typeOf.js
@@ -8,3 +8,22 @@ const isTypeOf = (o) => {
     let r = key.split(' ')[1].split(']')[0]
     return r
 }
+
+// compare with result of isTypeOf func
+class JSType {
+    static function() {
+        return 'Function'
+    }
+    static array() {
+        return 'Array'
+    }
+    static string() {
+        return 'String'
+    }
+    static regexp() {
+        return 'Regexp'
+    }
+    static number() {
+        return 'Number'
+    }
+}
